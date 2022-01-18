2eb411b81b936f912bb0d4da77a9cd7facb6c83d
Faisal Nazik <faisalnazik@gmail.com>
2022-01-18 23:21:28 +0500

    falisfy_message

--- verify_message.py
+++ verify_message.py
@@ -4,6 +4,13 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 
+# Generate private key
+#private_key = rsa.generate_private_key(
+# public_exponent=65537,
+# key_size=2048,
+# backend=default_backend()
+#)
+
 # Configuration
 GENERATE_PRIVATE_KEY = False
 DERIVE_PUBLIC_KEY_FROM_PRIVATE_KEY = False
